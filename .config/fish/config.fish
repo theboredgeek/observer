0ccccda864729d343d4479269eca75bd4f3daded
theboredgeek <theboredgeek@gmail.com>
2026-04-03 17:11:35 -0700

    added swaync to matugen and congfigured look/feel to match system. swaync is still barebones, but it looks better

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Initialize Starship
 starship init fish | source
 
+thefuck --alias | source
+
 # Overwrite greeting (cleans up the terminal start)
 function fish_greeting
 end
