1f16db6ec3f8a0bfb5bae6a288a07cae56be406c
theboredgeek <theboredgeek@gmail.com>
2026-07-22 01:25:18 -0700

    edited animations.lua for faster speeds

--- observer/.config/hypr/config/animations.lua
+++ observer/.config/hypr/config/animations.lua
@@ -1,20 +1,20 @@
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
 
 -- Default beziers
-hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+hl.curve("easeOutQuint",     { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
-hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
-hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
-hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
-hl.curve("overshoot",      { type = "bezier", points = { {0.5, 0.9}, {0.1, 1.1}     } })
+hl.curve("linear",           { type = "bezier", points = { {0, 0},        {1, 1}       } })
+hl.curve("almostLinear",     { type = "bezier", points = { {0.5, 0.5},    {0.75, 1}    } })
+hl.curve("quick",            { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+hl.curve("overshoot",        { type = "bezier", points = { {0.5, 0.9}, {0.1, 1.1}     } })
 
 -- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-hl.curve("rubber",         { type = "spring", mass = 1, stiffness = 70,      dampening = 10         })
+hl.curve("easy",             { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+hl.curve("rubber",           { type = "spring", mass = 1, stiffness = 70,      dampening = 10         })
 
--- Animations
-hl.animation({ leaf = "global",              enabled = true, speed = 1, bezier = "quick"                 })
-hl.animation({ leaf = "windows",             enabled = true, speed = 1, spring = "easy",  style = "slide" })
-hl.animation({ leaf = "workspaces",          enabled = true, speed = 1, bezier = "quick", style = "slide" })
-hl.animation({ leaf = "specialWorkspaceIn",  enabled = true, speed = 1, bezier = "quick", style = "slide top"})
-hl.animation({ leaf = "specialWorkspaceOut", enabled = true, speed = 1, bezier = "quick", style = "slide bottom"})
+-- Optimized Fast Animations (Speed set to 1.5 or 2 for a snappy feel)
+hl.animation({ leaf = "global",              enabled = true, speed = 1.5, bezier = "quick"                 })
+hl.animation({ leaf = "windows",             enabled = true, speed = 1.5, bezier = "quick",  style = "popin 90%" })
+hl.animation({ leaf = "workspaces",          enabled = true, speed = 1.5, bezier = "quick",  style = "slide" })
+hl.animation({ leaf = "specialWorkspaceIn",  enabled = true, speed = 1.5, bezier = "quick",  style = "slide top"})
+hl.animation({ leaf = "specialWorkspaceOut", enabled = true, speed = 1.5, bezier = "quick",  style = "slide bottom"})
