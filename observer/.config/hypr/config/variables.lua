c84fa206fd5888ea36afe22f96a01b473a3b5fd3
theboredgeek <theboredgeek@gmail.com>
2026-07-22 00:29:47 -0700

    changed variables.lua to attempt workspace fix

--- observer/.config/hypr/config/variables.lua
+++ observer/.config/hypr/config/variables.lua
@@ -7,11 +7,21 @@ EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
 
 -- Monitors
-MONITOR1 = "DP-1"
-MONITOR2 = "DP-2"
-MONITOR3 = "DP-3"
-MONITOR4 = "HDMI-A-1"
-PRIMARY_MONITOR = MONITOR1
+local hostname = io.popen("uname -n"):read("*l")
+
+if hostname == "your-laptop-hostname" then
+    -- Fallback variables for your laptop so global references don't crash
+    MONITOR1 = "eDP-1"
+    MONITOR2 = "eDP-1"
+    MONITOR3 = "eDP-1"
+    PRIMARY_MONITOR = "eDP-1"
+else
+    -- Your desktop variables
+    MONITOR1 = "DP-1"
+    MONITOR2 = "DP-2"
+    MONITOR3 = "DP-3"
+    PRIMARY_MONITOR = "DP-1"
+end
 
 -- Workspaces
 NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
